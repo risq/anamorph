b6b213056d184a0217bcb87cafa04700da4a7c97
Ivolvy <michael.genty@free.fr>
2016-04-13 16:00:50 +0200

    [Instagram] - Added connection to api and begin to get liked medias

--- src/server/instagramDataFetcher.js
+++ src/server/instagramDataFetcher.js
@@ -9,47 +9,73 @@ const dbg = require('debug')('anamorph:instagramDataFetcher');
 // const instagram = bluebird.promisifyAll(require('instagram-node').instagram());
 
 module.exports = class InstagramDataFetcher {
-  constructor(clientId, code) {
-    this.gram = new Nodegram({
-      clientId: '208f9dfdb4b44a228b4c7f95b56bc58e',
-      clientSecret: '00afe917a374431296dcc65bd645fccf',
-      redirectUri: `http://localhost:3000/insta?clientId=${clientId}`,
-    });
-    this.gram.getAuthUrl();
-    this.gram.getAccessToken(code)
-      .then(res => {
-        dbg('getAccessToken', res);
-      }).catch(err => dbg(err));
+    constructor(clientId, code) {
+        this.clientId = clientId
+        this.code = code;
 
-    this.data = {
-      name: '',
-      posts: [],
-    };
-  }
+        this.data = {
+            name: '',
+            posts: [],
+        };
+    }
 
-  authenticate(code) {
-    this.gram.getAccessToken(code)
-      .then(res => dbg(res));
-  }
+    authenticate() {
+        dbg('authenticate process');
+        const nodegram = new Nodegram({
+            clientId: '208f9dfdb4b44a228b4c7f95b56bc58e',
+            clientSecret: '00afe917a374431296dcc65bd645fccf',
+            redirectUri: `http://localhost:3000/insta?clientId=${this.clientId}`,
+        });
 
-  fetch() {
-    const data = {};
-    // return this.fetchTest()
-    //   .then(() => this.data)
-    //   .catch(err => dbg(`Error: ${err.message}`));
-    return bluebird.resolve(true);
-  }
+        nodegram.getAuthUrl();
 
-  fetchTest(){
-    // return instagram.user_searchAsync('username').then(() => {
-    //
-    //     this.data.test = "zzz";
-    //     console.log(arguments);
-    //   dbg("eee");
-    // }).catch(function(e) {
-    //   console.error(e);
-    // });
+        return nodegram.getAccessToken(this.code)
+            .then(res => res.access_token);
+    }
 
-  }
+    fetch() {
+        dbg('fetch process');
+        return this.authenticate()
+            .then((accessToken) => {
 
+                this.gram = new Nodegram({accessToken});
+                this.gram.getAuthUrl();
+                return this.fetchNumberOfUserLikes()
+                    .then(() => dbg(this.data.numberOfUserLikes))
+                    .catch(err => dbg(`Error: ${err.message}`));
+            });
+    }
+
+    fetchNumberOfUserLikes(url){
+        dbg('fetchNumberOfUserLikes');
+        this.gram.get('/users/self/media/liked',{count: 2000}).then(this.onSuccess).catch(this.onError);
+
+      /*  url = url || `/users/self/media/liked`;
+        return this.gram.get(url, {max_like_id: 2000, count: 2000})
+            .then((res, pag) => {
+                dbg(res);
+                dbg('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+                dbg(pag);
+                dbg(res.length);
+
+                dbg('nextUrl:', res.pagination.next_url);
+                if (res.pagination && res.pagination.next_url) {
+                    return this.fetchNumberOfUserLikes(res.pagination.next_url);
+                } else {
+                    this.data.numberOfUserLikes = res.length;
+                }
+            })
+            .catch(err => dbg(`Error: ${err.message}`));*/
+
+    }
+     onSuccess(res, pag) {
+         dbg('aaaaaaaaaaaa');
+        //console.log('onSuccess', res, pag);
+         dbg(res.length);
+    }
+
+     onError(err) {
+         dbg('bbbbbbbbbb');
+        console.log('onError', err);
+    }
 };
