cf4461c6f58a7d1272e6d92a427f1b7c60e79c13
Ivolvy <michael.genty@free.fr>
2016-05-11 10:37:39 +0200

    [Instagram] - Added posts frequency

--- src/server/instagramDataFetcher.js
+++ src/server/instagramDataFetcher.js
@@ -16,7 +16,9 @@ module.exports = class InstagramDataFetcher {
       averageOfGetLikes: [],
       averageOfGetComments: [],
       averageOfTagsForPostPublication: [],
+      frequency: [],
     };
+    this.datePosts = [];
   }
 
   fetch() {
@@ -31,6 +33,7 @@ module.exports = class InstagramDataFetcher {
       .then(() => this.fetchAverageOfGetLikes())
       .then(() => this.fetchAverageOfGetComments())
       .then(() => this.fetchAverageTagsForPostPublication())
+      .then(() => this.fetchPostsFrequency())
       .then(() => this.data)
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -41,6 +44,7 @@ module.exports = class InstagramDataFetcher {
     return this.gram.get('/users/self/', {})
       .then(res => {
         this.data.numberOfUserPublications = res.counts.media;
+          dbg(`Number of user publications: ${this.data.numberOfUserPublications}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -51,6 +55,7 @@ module.exports = class InstagramDataFetcher {
     return this.gram.get('/users/self/', {})
       .then(res => {
         this.data.numberOfUserFollowers = res.counts.followed_by;
+          dbg(`Number of user followers: ${this.data.numberOfUserFollowers}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -61,6 +66,7 @@ module.exports = class InstagramDataFetcher {
     return this.gram.get('/users/self/', {})
       .then(res => {
         this.data.numberOfUserFollows = res.counts.follows;
+          dbg(`Number of user follows: ${this.data.numberOfUserFollows}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -76,6 +82,7 @@ module.exports = class InstagramDataFetcher {
         res.forEach(res => numberOfLikes += res.likes.count);
 
         this.data.averageOfGetLikes = Math.round(numberOfLikes / numberOfPublications);
+          dbg(`Average of likes: ${this.data.averageOfGetLikes}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -92,6 +99,7 @@ module.exports = class InstagramDataFetcher {
         res.forEach(res => numberOfComments += res.comments.count);
 
         this.data.averageOfGetComments = Math.round(numberOfComments / numberOfPublications);
+          dbg(`Average of comments: ${this.data.averageOfGetComments}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
@@ -107,7 +115,92 @@ module.exports = class InstagramDataFetcher {
         res.forEach(res => numberOfTags += res.tags.length);
 
         this.data.averageOfTagsForPostPublication = Math.round(numberOfTags / numberOfPublications);
+          dbg(`Average of tags for post publication: ${this.data.averageOfTagsForPostPublication}`);
       })
       .catch(err => dbg(`Error: ${err.message}`));
   }
+
+
+  fetchPostsFrequency() {
+    dbg('Retrieving posts frequency');
+
+    return this.gram.get('/users/self/media/recent', {})
+        .then(res => {
+
+          res.forEach((data => {
+            var date = new Date(data.created_time*1000);
+            var year = 'A-'+date.getFullYear();
+
+            if(typeof(this.datePosts[year]) != 'undefined'){
+              this.datePosts[year]+= 1;
+            }else{
+              this.datePosts[year] = 0;
+              this.datePosts[year]+= 1;
+            }
+          }));
+
+          //Calculate the number of day since the beginning of the current year
+          var now = new Date();
+          var start = new Date(now.getFullYear(), 0, 0);
+          var diff = now - start;
+          var oneDay = 1000 * 60 * 60 * 24;
+          var day = Math.floor(diff / oneDay);
+
+          //Todo -> improved this recuperation - iteration?
+          this.data.frequency['A-2016']= (this.datePosts['A-2016'] /day).toFixed(3);
+          this.data.frequency['A-2015']= (this.datePosts['A-2015'] /365).toFixed(3);
+
+          dbg('Frequency');
+          dbg(this.data.frequency);
+
+        })
+        .catch(err => dbg(`Error: ${err.message}`));
+  }
+
+
+  fetchPostsFrequencyTemp(url) {
+
+    return this.get(url || '/me/feed', {
+      limit: 100,
+    }).then(res => {
+
+      this.tempDatePosts.push(...res.data);
+
+      if (res.paging && res.paging.next) {
+        return this.fetchPostsFrequency(res.paging.next);
+      }
+      else{
+        dbg('Fetching posts frequency');
+
+        this.tempDatePosts.forEach((data => {
+          var year = 'A-'+data.created_time.substr(0, 4).toString();
+
+          if(typeof(this.datePosts[year]) != 'undefined'){
+            this.datePosts[year]+= 1;
+          }else{
+            this.datePosts[year] = 0;
+            this.datePosts[year]+= 1;
+          }
+        }));
+
+        //Calculate the number of day since the beginning of the current year
+        var now = new Date();
+        var start = new Date(now.getFullYear(), 0, 0);
+        var diff = now - start;
+        var oneDay = 1000 * 60 * 60 * 24;
+        var day = Math.floor(diff / oneDay);
+
+        //Todo -> improved this recuperation - iteration?
+        this.data.frequency['A-2016']= (this.datePosts['A-2015'] /day).toFixed(3);
+        this.data.frequency['A-2015']= (this.datePosts['A-2015'] /365).toFixed(3);
+
+        dbg('Frequency');
+        dbg(this.data.frequency);
+      }
+
+      return Bluebird.resolve(true); // TODO
+    });
+  }
+
+
 };
