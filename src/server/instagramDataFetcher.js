0ad7003a55eb10a30e70c9d09461402e3da21cee
Ivolvy <michael.genty@free.fr>
2016-05-12 14:59:00 +0200

    [Instagram] - Added most used hashtags

--- src/server/instagramDataFetcher.js
+++ src/server/instagramDataFetcher.js
@@ -17,6 +17,7 @@ module.exports = class InstagramDataFetcher {
       averageOfGetComments: [],
       averageOfTagsForPostPublication: [],
       frequency: [],
+      mostUsedWords: [],
     };
     this.datePosts = [];
   }
@@ -108,18 +109,63 @@ module.exports = class InstagramDataFetcher {
     dbg('Retrieving average of tags used per publication');
     let numberOfPublications = 0;
     let numberOfTags = 0;
+    var wordList = [];
 
     return this.gram.get('/users/self/media/recent', {})
       .then(res => {
-        numberOfPublications += res.length;
-        res.forEach(res => numberOfTags += res.tags.length);
+          numberOfPublications += res.length;
+          res.forEach((res => {
+            numberOfTags += res.tags.length;
 
-        this.data.averageOfTagsForPostPublication = Math.round(numberOfTags / numberOfPublications);
+            res.tags.forEach((result => {
+              wordList.push(result);
+            }));
+          }));
+
+          this.data.averageOfTagsForPostPublication = Math.round(numberOfTags / numberOfPublications);
           dbg(`Average of tags for post publication: ${this.data.averageOfTagsForPostPublication}`);
-      })
+
+
+          //Get the most used hashtags
+          var wordAssociation = this.getWordFrequency(wordList);
+          for(var i=0; i<3; i++){
+            this.data.mostUsedWords.push(wordAssociation[i]);
+          }
+          dbg('Most used hashtags:');
+          dbg(this.data.mostUsedWords);
+
+        })
       .catch(err => dbg(`Error: ${err.message}`));
   }
 
+  //Get word frequency for used hashtags and sort them in array (bigger to smaller)
+  getWordFrequency(wordList){
+    var a = [], b = [], prev;
+
+    wordList.sort();
+    for ( var i = 0; i < wordList.length; i++ ) {
+      if ( wordList[i] !== prev ) {
+        a.push(wordList[i]);
+        b.push(1);
+      } else {
+        b[b.length-1]++;
+      }
+      prev = wordList[i];
+    }
+
+    //Associate the words with their occurrence
+    var associated = a.reduce(function (previous, key, index) {
+      previous[key] = b[index];
+      return previous
+    }, {});
+
+    //Sort and reverse the associated array
+    var tupleArray = [];
+    for (var key in associated) tupleArray.push([key, associated[key]]);
+    tupleArray.sort(function (a, b) { return a[1] - b[1] }).reverse();
+
+    return tupleArray;
+  }
 
   fetchPostsFrequency() {
     dbg('Retrieving posts frequency');
