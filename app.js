28c2c63a5752f11b75a97fd2d5583e404ed7a7ac
Ivolvy <michael.genty@free.fr>
2016-06-03 11:31:47 +0200

    [General] - Close automatically network connection tab

--- app.js
+++ app.js
@@ -25,25 +25,28 @@ app.get('/client/\\d+', (req, res) => {
   res.sendFile(path.join(__dirname, './public', 'client.html'));
 });
 
-
 app.get('/facebook', (req, res) => {
     clientManager.getClient(req.query.clientId)
         .onFacebookAuthResponse(req.query.code);
+    res.sendFile(path.join(__dirname, './public', 'close.html'));
 });
 
 app.get('/insta', (req, res) => {
   clientManager.getClient(req.query.clientId)
     .onInstagramAuthResponse(req.query.code);
+    res.sendFile(path.join(__dirname, './public', 'close.html'));
 });
 
 app.get('/twitter', (req, res) => {
   clientManager.getClient(req.query.clientId)
     .onTwitterAuthResponse(req.query.oauth_verifier);
+    res.sendFile(path.join(__dirname, './public', 'close.html'));
 });
 
 app.get('/linkedin', (req, res) => {
    clientManager.getClient(req.query.clientId)
     .onLinkedinAuthResponse(req.query.code, req.query.state);
+    res.sendFile(path.join(__dirname, './public', 'close.html'));
 });
 
 app.get('/validConnections', (req, res) => {
